0c2843deb50a0264544e32e47781ff239fa83352
RIN67630 <lazlo.lebrun@googlemail.com>
2020-11-06 06:40:19 +0100

    Update ESP_Solar_SAK.ino

--- ESP_Solar_SAK.ino
+++ ESP_Solar_SAK.ino
@@ -3,5 +3,5 @@ Solar Swiss Army Knife
 The Allrounder in Reporting for Solar SCCs and converting plain buck converters into MPPT Solar Chargers.
 
 Open source software by RIN67630
-License CC SA BY.
+License CC BY SA 3.0.
 */
