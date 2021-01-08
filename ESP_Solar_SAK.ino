21cae55a0194a2a08f327efb22ccf2d0f4efa937
RIN67630 <lazlo.lebrun@googlemail.com>
2021-01-08 22:41:04 +0100

    Update and rename ESP_Solar_SAK.ino to SoftPowerMPPT.ino

--- ESP_Solar_SAK.ino
+++
@@ -1,8 +0,0 @@
-*/
-Solar Swiss Army Knife
-The Allrounder in Reporting for Solar SCCs and converting plain buck converters into MPPT Solar Chargers.
-
-Open source software by RIN67630
-License CC BY SA 3.0.
-Hosted: https://github.com/rin67630/Solar-Booster/
-*/
