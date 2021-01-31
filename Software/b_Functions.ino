12e0e1385a4930b3525bb1ecf49d914df05a8829
RIN67630 <lazlo.lebrun@googlemail.com>
2021-01-31 22:41:38 +0100

    Update b_Functions.ino

--- Software/b_Functions.ino
+++ Software/b_Functions.ino
@@ -5,8 +5,7 @@
 
 // Instantiate classes
 WiFiUDP UDP;
-INA_Class INA1;
-INA_Class INA2;
+INA_Class INA;
 
 #if defined(THINGER)
 ThingerESP8266 thing(THINGER_USERNAME, THINGER_DEVICE, THINGER_CREDENTIALS);
@@ -99,8 +98,76 @@ void getTimeData()
   Day       = timeinfo->tm_mday;
   Month     = timeinfo->tm_mon + 1;
   Year      = timeinfo->tm_year + 1900; //returns years since 1900
-  strftime (DayName , 12, "%A", timeinfo); //cf: https://www.cplusplus.com/reference/ctime/strftime/
+  strftime (DayName,   12, "%A", timeinfo); //cf: https://www.cplusplus.com/reference/ctime/strftime/
   strftime (MonthName, 12, "%B", timeinfo);
-  strftime (Time, 10, "%T", timeinfo);
-  strftime (Date, 12, "%d/%m/%Y", timeinfo);
+  strftime (Time,      10, "%T", timeinfo);
+  strftime (Date,      12, "%d/%m/%Y", timeinfo);
+}
+
+int do__perturb_and_observe_mppt() { // performs Perturb & Observe MPPT; changes pwm_value to maximise power
+  if (dashboard.Wbat < last_power) { // power has gone down - change direction
+    aux_delta = - aux_delta;
+  }       // "else" power is the same or increased  - keep direction
+  dashboard.Wbat < last_power;
+  aux_injection += aux_delta;
+  aux_injection = constrain (aux_injection, 0, 255);
+  analogWrite (PWM_AUX, aux_injection);
+}
+
+int do__incremental_conductance_mppt() { // performs Inc Conductance MPPT; changes pwm_value to maximise power
+//place code here
+}
+
+int do_CV () { // performs fix voltage operation at target
+  if (dashboard.Vbat > aux_target) {
+    aux_delta = abs(aux_delta);
+  } else {                // decrease pwm value
+    aux_delta = - abs(aux_delta);
+  }
+  aux_injection += aux_delta;
+  aux_injection = constrain (aux_injection, 0, 1024);
+  analogWrite (PWM_AUX, aux_injection);
+}
+
+int do_CC () { // performs fix current operation at target
+  if (dashboard.Ibat > aux_target) { // increase pwm value
+    aux_delta = abs(aux_delta);
+  } else {                // decrease pwm value
+    aux_delta = - abs(aux_delta);
+  }
+  aux_injection += aux_delta;
+  aux_injection = constrain (aux_injection, 0, 1024);
+  analogWrite (PWM_AUX, aux_injection);
+}
+
+float pwm2volt (float pwm)
+{
+  int volt = map(pwm, 1024, 0, long(MIN_VOLT * 100), long(MAX_VOLT * 100));
+  return volt;
+}
+
+float volt2pwm (float volt)
+{
+  int pwm = map(volt, long(MIN_VOLT * 100), long(MAX_VOLT * 100), 1024, 0);
+  return pwm;
+}
+
+/*boolean time_phase()
+  {
+  phase_timer ++;
+  phase_duration[phase] = phase_timer;
+  expired = false;
+  if (phase_timer > phase_limit[phase]) expired = true;
+  return expired;
+  }*/
+
+void do_CVCC () {   // stay at or below voltage and current targets
+  if (dashboard.Ibat > scc_target || dashboard.Vbat > aux_target) { // increase pwm value to decrease charging
+    aux_delta = abs(aux_delta);
+    aux_injection += aux_delta;
+    aux_injection = constrain (aux_injection, 0, 255);
+    analogWrite (PWM_AUX, aux_injection);
+  } else {                // increase charging subject to capacity
+    do__perturb_and_observe_mppt();
+  }
 }
