648a1725bcbdf143e5c7fdde085dc3918f41f33f
RIN67630 <lazlo.lebrun@googlemail.com>
2021-01-31 22:44:56 +0100

    Update f_Display.ino

--- Software/f_Display.ino
+++ Software/f_Display.ino
@@ -1,4 +1,51 @@
 void displayRun()
 {
     yield(); 
+
+#ifdef DISPLAY_IS_OLED64x48
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 0,  "Panl =" +String(dashboard.Vpan)   +"V");
+    display.drawString(0, 12, "Batt  ="+String(dashboard.Vbat) +"V");
+//    display.drawString(0, 24, "Curr  ="+String(dashboard.Ibat) +"A");
+    sprintf(charbuff, "Curr  =%1.3fA", dashboard.Ibat); display.drawString(0,24, charbuff);
+    display.drawString(0, 36, "Powr="  +String(dashboard.Wbat)   +"W");
+    display.display();
+#endif
+#ifdef DISPLAY_IS_OLED128x64
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(10, 0,  "|  Panel");  
+    display.drawString(50, 0,  "| Battery");
+    display.drawString(90, 0,  "| Output");
+    display.drawString(0, 12, "V ");
+    sprintf(charbuff, "| %2.2f", dashboard.Vpan); display.drawString(10,12, charbuff);
+    sprintf(charbuff, "| %2.2f", dashboard.Vbat); display.drawString(50,12, charbuff);
+    sprintf(charbuff, "| %2.2f", dashboard.Vaux); display.drawString(90,12, charbuff);
+    display.drawString(0, 24, "A ");
+    sprintf(charbuff, "| %1.3f", dashboard.Ipan); display.drawString(10,24, charbuff);
+    sprintf(charbuff, "| %1.3f", dashboard.Ibat); display.drawString(50,24, charbuff);
+    sprintf(charbuff, "| %1.3f", dashboard.Iaux); display.drawString(90,24, charbuff);
+    display.drawString(0, 36, "W ");
+    sprintf(charbuff, "| %2.2f", dashboard.Wpan); display.drawString(10,36, charbuff);
+    sprintf(charbuff, "| %2.2f", dashboard.Wbat); display.drawString(50,36, charbuff);
+    sprintf(charbuff, "| %2.2f", dashboard.Waux); display.drawString(90,36, charbuff);
+        if (dashboard.Wpan > 0.03)
+    {
+      display.drawString(0, 48, "Efficiency: ");
+      sprintf(charbuff, "%2.2f %%", dashboard.efficiency); 
+      display.setFont(ArialMT_Plain_16);
+      display.drawString(50,45, charbuff); 
+    }
+    else
+    {
+      display.drawString(0, 48, "Not Loading  ");
+      display.setFont(ArialMT_Plain_16);
+      display.drawString(50,45, "           ");      
+    }
+    display.display();
+  
+#endif
 }
