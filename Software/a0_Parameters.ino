47607e73428c69f0218f95c4de3402ec73812fbf
RIN67630 <lazlo.lebrun@googlemail.com>
2021-02-04 21:17:46 +0100

    Update a0_Parameters.ino

--- Software/a0_Parameters.ino
+++ Software/a0_Parameters.ino
@@ -1,18 +1,18 @@
-#define HOST_NAME   "Soft-Power" // n.b: The HOST_NAME must match the device name of Thinger.
+#define HOST_NAME   "SoftPower" // n.b: The HOST_NAME must match the device name of Thinger.
 
-//  ***Credentials***  (Adjust to your own values)
-// Wifi
-#define SMARTCONFIG  // (WiFi Credentials over GogglePlay/Apple App SmartConfig)
-// alternatively to Smartconfig App, you can comment out Smartconfig 
-// and enter your WiFi credentials to initalize for a new WiFi
+//  ***Credentials***  
+// WifI: for the very first run with a new ESP or on a new WiFi network 
+// remove the // from the next line and provide real SSID and PASS credentials.
 //#define ERASE_WIFI_CREDENTIALS 
-#define WIFI_SSID            "SSID"
-#define WIFI_PASS            "PASS"
-#define wifiMaxTries         10
-#define wifiRepeatInterval   100
+#define WIFI_SSID          "SSID"
+#define WIFI_PASS          "Password"
+#define wifiMaxTries       20
+#define wifiRepeatInterval 500
+// after a successful connection, you can revert to the defaults lines again.
+
 //Thinger
 #define THINGER_USERNAME           "SoftPower1"       
-#define THINGER_DEVICE_CREDENTIALS "MyVerySecretDevicePassword"    
+#define THINGER_DEVICE_CREDENTIALS "Password"    
 #define THINGER_DEVICE HOST_NAME    
 //Location for weather
 #define OPEN_WEATHER_MAP_APP_ID      "208085abb5a3859d1e32341d6e1f9079" 
@@ -20,9 +20,9 @@
 #define OPEN_WEATHER_MAP_LANGUAGE    "en"
 #define OPEN_WEATHER_MAP_UNITS       "metric"
 //Time zone
-#define NTP_SERVER "th.pool.ntp.org"
-#define MYTZ TZ_Asia_Bangkok
-#define TZ   5               // (utc+) TZ in hours
+#define NTP_SERVER "de.pool.ntp.org"
+#define MYTZ TZ_Europe_Berlin
+#define TZ   1               // (utc+) TZ in hours
 
 // ***Hardware Configuration***  (Adjust to your own hardware options)
 #define WEATHER_SOURCE_IS_URL   //_URL _NONE         Change end accordingly
@@ -43,10 +43,10 @@
 #define AMPERE2   2         // Chose here about 2x the max expected Amps
 
 // ***Calibration***  (Leave as is for a first run)
-#define IFACTORP  1000000   // Panel Current correction factor 1000000 is normal,    -"- only used when PAN_SOURCE_IS_INA.
+#define IFACTORP  1000000   // Panel Current correction factor 1000000 is normal,   -"- only used when PAN_SOURCE_IS_INA.
 #define IFACTORB  1000000   // Battery Current correction factor 1000000 is normal,  -1000000 reverses shunt, change value to correct for wrong Amp values
-#define IFACTORA  1000000   // Aux Out Current correction factor 1000000 is normal,  -"- only used when AUX_SOURCE_IS_INA.
-#define PANEL_MAX 23300     // mV panel voltage  (Adjust to match panel voltage),    -only used when PAN_SOURCE_IS_A0.
+#define IFACTORA  1000000   // Aux Out Current correction factor 1000000 is normal,   -"- only used when AUX_SOURCE_IS_INA.
+#define PANEL_MAX 23300     // mV panel voltage  (Adjust to match panel voltage)
 #define INJ_NEUTRAL 430
 #define INJ_HP_MIN  00000   //   Minimum Battery Voltage Setpoint at injection=1024
 #define INJ_HP_MAX  00000   //   Maximum Battery Voltage Setpoint at injection=0 
