2cee56a62571b1dd6de86436dd3cc331d84568cf
RIN67630 <lazlo.lebrun@googlemail.com>
2021-03-19 20:13:13 +0100

    Update f_Display.ino

--- Software/f_Display.ino
+++ Software/f_Display.ino
@@ -8,9 +8,9 @@ void displayRun()
   display.setFont(ArialMT_Plain_10);
   display.drawString(0, 0,  "Panl =" + String(dashboard.Vpan)   + "V");
   display.drawString(0, 12, "Batt  =" + String(dashboard.Vbat) + "V");
-  //    display.drawString(0, 24, "Curr  ="+String(dashboard.Iin) +"A");
-  sprintf(charbuff, "Curr  =%1.3fA", dashboard.Iin); display.drawString(0, 24, charbuff);
-  display.drawString(0, 36, "Powr="  + String(dashboard.Win)   + "W");
+  //    display.drawString(0, 24, "Curr  ="+String(dashboard.Ibat) +"A");
+  sprintf(charbuff, "Curr  =%1.3fA", dashboard.Ibat); display.drawString(0, 24, charbuff);
+  display.drawString(0, 36, "Powr="  + String(dashboard.Wbat)   + "W");
   display.display();
 #endif
 #ifdef DISPLAY_IS_OLED128x64
@@ -35,12 +35,12 @@ void displayRun()
   sprintf(charbuff, "| %2.2f", dashboard.Vaux); display.drawString(90, 12, charbuff);
   display.drawString(0, 24, "A ");
   sprintf(charbuff, "| %1.3f", dashboard.Ipan); display.drawString(10, 24, charbuff);
-  sprintf(charbuff, "| %1.3f", dashboard.Iin); display.drawString(50, 24, charbuff);
-  sprintf(charbuff, "| %1.3f", dashboard.Iout); display.drawString(90, 24, charbuff);
+  sprintf(charbuff, "| %1.3f", dashboard.Ibat); display.drawString(50, 24, charbuff);
+  sprintf(charbuff, "| %1.3f", dashboard.Iaux); display.drawString(90, 24, charbuff);
   display.drawString(0, 36, "W ");
   sprintf(charbuff, "| %2.2f", dashboard.Wpan); display.drawString(10, 36, charbuff);
-  sprintf(charbuff, "| %2.2f", dashboard.Win); display.drawString(50, 36, charbuff);
-  sprintf(charbuff, "| %2.2f", dashboard.Wout); display.drawString(90, 36, charbuff);
+  sprintf(charbuff, "| %2.2f", dashboard.Wbat); display.drawString(50, 36, charbuff);
+  sprintf(charbuff, "| %2.2f", dashboard.Waux); display.drawString(90, 36, charbuff);
 
   buffTimeData();
   display.drawString(0, 50, charbuff);
